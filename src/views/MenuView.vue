c3db4622dd927e2b27822d7e5992979c805d306a
Mizu <psdk55052004@gmail.com>
2022-11-07 10:56:46 +0800

    修改

--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -14,12 +14,21 @@ export default {
     }
   },
   mounted () {
+    // window.addEventListener('onorientationchange' in window ? 'orientationchange' : 'resize', function () {
+    //   if (window.orientation === 180 || window.orientation === 0) {
+    //     this.resolut = '目前您的螢幕為縱向！'
+    //   }
+    //   if (window.orientation === 90 || window.orientation === -90) {
+    //     this.resolut = '目前您的螢幕為橫向！'
+    //   }
+    // }, false)
+
     window.addEventListener('onorientationchange' in window ? 'orientationchange' : 'resize', function () {
       if (window.orientation === 180 || window.orientation === 0) {
-        this.resolut = '目前您的螢幕為縱向！'
+        alert('目前您的螢幕為縱向！')
       }
       if (window.orientation === 90 || window.orientation === -90) {
-        this.resolut = '目前您的螢幕為橫向！'
+        alert('目前您的螢幕為橫向！')
       }
     }, false)
   }
