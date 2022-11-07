4f27c50568d65d148863b71cade51ba6e9b1a8e9
Mizu <psdk55052004@gmail.com>
2022-11-07 10:51:14 +0800

    修改

--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,18 +1,25 @@
 <template>
   <h2>查看手機目前是直向或橫向</h2>
 
-  <h3 ref="resolut"></h3>
+  <h3>
+    {{ resolut }}
+  </h3>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      resolut: '螢幕橫縱向偵測'
+    }
+  },
   mounted () {
     window.addEventListener('onorientationchange' in window ? 'orientationchange' : 'resize', function () {
       if (window.orientation === 180 || window.orientation === 0) {
-        this.$refs.resolut.textContent = '目前您的螢幕為縱向！'
+        this.resolut = '目前您的螢幕為縱向！'
       }
       if (window.orientation === 90 || window.orientation === -90) {
-        this.$refs.resolut.textContent = '目前您的螢幕為橫向！'
+        this.resolut = '目前您的螢幕為橫向！'
       }
     }, false)
   }
