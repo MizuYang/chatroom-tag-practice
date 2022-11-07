0a703c5600cf43b6c9c9049282bce4cb22cfe655
Mizu <psdk55052004@gmail.com>
2022-11-07 10:08:07 +0800

    修改 將監聽改到生命週期

--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -5,15 +5,16 @@
 </template>
 
 <script>
-
-window.addEventListener('onorientationchange' in window ? 'orientationchange' : 'resize', function () {
-  if (window.orientation === 180 || window.orientation === 0) {
-    this.$refs.resolut.textContent = '目前您的螢幕為縱向！'
-  }
-  if (window.orientation === 90 || window.orientation === -90) {
-    this.$refs.resolut.textContent = '目前您的螢幕為橫向！'
-  }
-}, false)
 export default {
+  mounted () {
+    window.addEventListener('onorientationchange' in window ? 'orientationchange' : 'resize', function () {
+      if (window.orientation === 180 || window.orientation === 0) {
+        this.$refs.resolut.textContent = '目前您的螢幕為縱向！'
+      }
+      if (window.orientation === 90 || window.orientation === -90) {
+        this.$refs.resolut.textContent = '目前您的螢幕為橫向！'
+      }
+    }, false)
+  }
 }
 </script>
